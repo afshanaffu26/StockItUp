b802dbb0a8fd6cb35d4067852d664df5c2fd57d6
Afshan Jabeen <afshanaffu26@gmail.com>
2020-09-10 18:39:52 -0400

    Added Splash screen, app icon and changed title bar color

---
+++ app/src/main/java/com/example/stockitup/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.stockitup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+
+/**
+ *  Manages application main content
+ */
+public class SplashActivity extends AppCompatActivity {
+    Window window;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        if(Build.VERSION.SDK_INT>=21){
+            window=this.getWindow();
+            window.setStatusBarColor(this.getResources().getColor(R.color.colorPrimary));
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent mainIntent =  new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+        },3000);
+    }
+}
