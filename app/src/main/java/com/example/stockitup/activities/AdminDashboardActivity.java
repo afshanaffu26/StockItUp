d4f04fbdb942f47a40438333a250edf01f6abf4b
Afshan Jabeen <afshanaffu26@gmail.com>
2020-09-22 17:48:01 -0400

    Admin dashboard screen UI and varioud required resources

--- app/src/main/java/com/example/stockitup/activities/AdminDashboardActivity.java
+++ app/src/main/java/com/example/stockitup/activities/AdminDashboardActivity.java
@@ -1,33 +1,41 @@
 package com.example.stockitup.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 import com.example.stockitup.R;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class AdminDashboardActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button btnLogOut;
+    CardView cardLogout;
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_dashboard);
         //teamstockitup@gmil.com
         //stockitup.8
-        btnLogOut = findViewById(R.id.btnLogOut);
-        btnLogOut.setOnClickListener(this);
+        cardLogout = findViewById(R.id.cardLogout);
+        cardLogout.setOnClickListener(this);
+
+        String appName = getApplicationContext().getResources().getString(R.string.app_name);
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(appName);
     }
 
     @Override
     public void onClick(View view) {
         switch (view.getId())
         {
-            case R.id.btnLogOut:
+            case R.id.cardLogout:
                 userLogout();
                 break;
         }
