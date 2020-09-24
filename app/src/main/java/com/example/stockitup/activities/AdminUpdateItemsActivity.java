382f9a24328a63db3c614ed8fe930d48233b0575
surya <gutpes@gmail.com>
2020-09-24 14:17:36 -0400

    Added AdminUpdateItemsActivity UI and populated item data

--- app/src/main/java/com/example/stockitup/activities/AdminUpdateItemsActivity.java
+++ app/src/main/java/com/example/stockitup/activities/AdminUpdateItemsActivity.java
@@ -1,14 +1,74 @@
 package com.example.stockitup.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import com.example.stockitup.R;
+import androidx.appcompat.widget.Toolbar;
 
-public class AdminUpdateItemsActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.example.stockitup.R;
+import com.squareup.picasso.Picasso;
+
+public class AdminUpdateItemsActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editName,editDesc,editPrice;
+    private String name,desc,price,image;
+    private ImageView imageView;
+    private ProgressBar progressBar;
+    private Button btnUpdate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_update_items);
+
+        String appName = getApplicationContext().getResources().getString(R.string.app_name);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(appName);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        editName = findViewById(R.id.editName);
+        editDesc = findViewById(R.id.editDesc);
+        editPrice = findViewById(R.id.editPrice);
+        imageView = findViewById(R.id.imageView);
+        progressBar = findViewById(R.id.progressBar);
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnUpdate.setOnClickListener(this);
+
+        name = getIntent().getStringExtra("name");
+        desc = getIntent().getStringExtra("desc");
+        price = getIntent().getStringExtra("price");
+        image = getIntent().getStringExtra("image");
+
+        editName.setText(name);
+        editDesc.setText(desc);
+        editPrice.setText(price);
+
+        if (image != "")
+            Picasso.get().load(image).into(imageView);
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId())
+        {
+            case R.id.btnUpdate:
+                updateItemData();
+                break;
+        }
+    }
+
+    private void updateItemData() {
+
     }
 }
