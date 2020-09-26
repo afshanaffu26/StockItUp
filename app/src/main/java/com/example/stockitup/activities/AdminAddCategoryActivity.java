ccd69ca095f01452b5fc2f915a0088daebfeb0cd
surya <gutpes@gmail.com>
2020-09-26 19:03:04 -0400

    Added AdminAddCategoryActivity UI and functionality

--- app/src/main/java/com/example/stockitup/activities/AdminAddCategoryActivity.java
+++ app/src/main/java/com/example/stockitup/activities/AdminAddCategoryActivity.java
@@ -1,15 +1,94 @@
 package com.example.stockitup.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import com.example.stockitup.R;
+import androidx.appcompat.widget.Toolbar;
 
-public class AdminAddCategoryActivity extends AppCompatActivity {
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.example.stockitup.R;
+import com.example.stockitup.models.CategoriesModel;
+import com.example.stockitup.utils.AppConstants;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
+
+public class AdminAddCategoryActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editName;
+    private Button btnAdd;
+    private ImageView imageViewEdit,imageView;
+    private String image,documentId,name;
+    private FirebaseFirestore firebaseFirestore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_add_category);
 
+        String appName = getApplicationContext().getResources().getString(R.string.app_name);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(appName);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        editName = findViewById(R.id.editName);
+        btnAdd = findViewById(R.id.btnAdd);
+        imageViewEdit = findViewById(R.id.imageViewEdit);
+        imageView = findViewById(R.id.imageView);
+
+        imageViewEdit.setOnClickListener(this);
+        btnAdd.setOnClickListener(this);
+
+        name = getIntent().getStringExtra("name");
+        image = getIntent().getStringExtra("image");
+        documentId = getIntent().getStringExtra("categoryDocumentId");
+
+        editName.setText(name);
+        if (image!=null && !image.isEmpty())
+            Picasso.get().load(image).into(imageView);
+
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        finish();
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId())
+        {
+            case R.id.btnAdd:
+                addCategory();
+                break;
+            case R.id.imageViewEdit:
+                break;
+        }
+    }
+
+    private void addCategory() {
+        String name,image;
+        name = editName.getText().toString();
+        image = "";
+        CategoriesModel categoriesModel = new CategoriesModel(name,image);
+        firebaseFirestore.collection(AppConstants.CATEGORY_COLLECTION)
+                .add(categoriesModel)
+                .addOnCompleteListener(new OnCompleteListener<DocumentReference>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentReference> task) {
+                        if (task.isSuccessful())
+                            Toast.makeText(getApplicationContext(),"Category Added",Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
